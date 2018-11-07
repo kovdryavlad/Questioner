7dc09d80749fa76fc7d6c8a45699e50fc796ddfe
Vladislav Kovdrya <kovdryavlad@gmail.com>
2018-11-07 19:41:24 +0200

    add parametr 'isCaseSensitive'

--- javascript/QAapp/ValidationServices/WithCorrectValuesValidationService.js
+++ javascript/QAapp/ValidationServices/WithCorrectValuesValidationService.js
@@ -1,10 +1,17 @@
 'use strict';
 
 class WithCorrectValuesValidationService{
-
+    
     isValid(answer, validationRule){
-        
-        return validationRule.correctAnswers.includes(answer);
+        const isCaseSensitive = validationRule.isCaseSensitive || false;
+
+        if(isCaseSensitive){
+            return validationRule.correctAnswers.includes(answer);
+        } else{
+            let correctValuesInLowRegister = validationRule.correctAnswers.map(str=>str.toUpperCase());
+            return correctValuesInLowRegister.includes(answer.toUpperCase());
+        }
+
     }
 }
 
