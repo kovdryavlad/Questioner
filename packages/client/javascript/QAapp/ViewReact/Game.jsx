a516a67848bc149159a8acef90d12a6c2ff68228
Vladislav Kovdrya <kovdryavlad@gmail.com>
2018-12-03 01:51:14 +0200

    create

---
+++ packages/client/javascript/QAapp/ViewReact/Game.jsx
@@ -0,0 +1,20 @@
+'use strict';
+
+const React = require('react');
+
+class Game extends React.Component{
+	
+	constructor(props) {
+		super(props);			
+	}
+
+	render()
+	{
+        return (<iframe width={this.props.width} height={this.props.height} src={this.props.gameLink}>
+                    Ваш браузер не поддерживает iframe
+                </iframe>);
+	}
+
+};
+
+module.exports = Game;
