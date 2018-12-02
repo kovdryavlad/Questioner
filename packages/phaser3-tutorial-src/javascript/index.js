9c7e241e3c4c2e9a90b735dda1c886f0ec8facd3
Vladislav Kovdrya <kovdryavlad@gmail.com>
2018-12-02 21:18:12 +0200

    part 3 done

--- packages/phaser3-tutorial-src/javascript/index.js
+++ packages/phaser3-tutorial-src/javascript/index.js
@@ -6,6 +6,15 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+
     scene: {
         preload: preload,
         create: create,
@@ -13,14 +22,31 @@ var config = {
     }
 };
 
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('sky', './sky.png');
+    this.load.image('ground', './platform.png');
+    this.load.image('star', './star.png');
+    this.load.image('bomb', './bomb.png');
+    this.load.spritesheet('dude','./dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
 }
 
+var platforms;
 function create ()
 {
+    this.add.image(400, 300, 'sky');
+    
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update ()
